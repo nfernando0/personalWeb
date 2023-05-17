48da05fa574bd3866c4954c9d9827aa1d022c97a
nfernando0 <nandof986@gmail.com>
2023-05-17 21:04:23 +0700

    menambah form

---
+++ script.js
@@ -0,0 +1,34 @@
+function submitData() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let subject = document.getElementById("subject").value;
+  let message = document.getElementById("msg").value;
+
+  if (name == "") {
+    return alert("Nama tidak boleh kosong!");
+  }
+  if (email == "") {
+    return alert("Email tidak boleh kosong!");
+  }
+  if (phone == "") {
+    return alert("Nomor HP tidak boleh kosong!");
+  }
+  if (subject == "") {
+    return alert("Subject tidak boleh kosong!");
+  }
+
+  let emailReceiver = "nandof986@gmail.com";
+  let a = document.createElement("a");
+
+  a.href = `mailto:${emailReceiver}?subject=${subject}&body=Hallo, nama saya ${name}, ${message}, tolong hubungi saya di nomor ${phone}, terima kasih`;
+  a.click();
+
+  let data = {
+    name,
+    email,
+    phone,
+    subject,
+    message,
+  };
+}
